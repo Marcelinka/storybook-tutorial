0f3fdea0df06fd649754eff81eac2e925d591d0a
Darina Syromyatnikova <d.syromyatnikova@darix.com>
2021-12-11 01:29:33 +0700

    feat: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  vueIndentScriptAndStyle: false,
+  // compatibility between windows and linux
+  endOfLine: 'auto',
+};

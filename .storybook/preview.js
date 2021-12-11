90c095e4b9fa829b84add2421398b965f6e99e26
Darina Syromyatnikova <d.syromyatnikova@darix.com>
2021-12-12 02:18:18 +0700

    feat: add console addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import '../src/index.css';
 
+// @see https://storybook.js.org/addons/@storybook/addon-console
+import '@storybook/addon-console';
+
 export const parameters = {
   //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
   actions: { argTypesRegex: '^on[A-Z].*' },

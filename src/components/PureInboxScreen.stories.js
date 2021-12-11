f7dbc944b3b17f35f84dee2105b569ec4348c98d
Darina Syromyatnikova <d.syromyatnikova@darix.com>
2021-12-11 18:21:53 +0700

    feat: add interactions

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,5 +1,6 @@
 import { app } from '@storybook/vue3';
 import { createStore } from 'vuex';
+import { fireEvent, within } from '@storybook/testing-library';
 
 import PureInboxScreen from './PureInboxScreen.vue';
 
@@ -39,3 +40,12 @@ export const Default = Template.bind({});
 
 export const Error = Template.bind({});
 Error.args = { error: true };
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+};
